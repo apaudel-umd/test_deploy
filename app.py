5b9541a3ff1831d280197f28d47c7256f291e030
Adhish <99695844+apaudel-umd@users.noreply.github.com>
2023-07-30 18:58:45 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,78 @@
+import configparser
+import psycopg2
+from shapely.geometry import MultiPoint
+from shapely.wkb import dumps
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__, static_url_path='')
+
+'''@app.route('/')
+def index():
+    return render_template('index.html')'''
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+    
+@app.route('/process_data', methods=['POST'])
+def process_data():
+    #data = request.json
+    #data = request.data.decode('utf-8')
+    data = request.json
+    print(data)
+    points = [(d['longitude'], d['latitude']) for d in data]
+     # Convert the list of points to a MultiPoint object
+    multipoint = MultiPoint(points)
+    # Convert the MultiPoint object to a WKB (Well-Known Binary) representation
+    wkb_multipoint = dumps(multipoint)
+
+    # Process the received data as needed
+    # ...
+    insert_loc('Flask', wkb_multipoint)
+    response_data = {'message': 'Data received successfully'}
+
+    return jsonify(response_data)
+
+def get_conn():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    params = {
+        'host': config.get('postgresql', 'host'),
+        'database': config.get('postgresql', 'database'),
+        'user': config.get('postgresql', 'user'),
+        'password': config.get('postgresql', 'password')
+    }
+
+    return psycopg2.connect(**params)
+
+def insert_loc(name, loc):
+    """ insert a new location into the multip_geom_data table """
+    insert_query = '''INSERT INTO multip_geom_data (name, geom) VALUES (%s, %s) RETURNING id;'''
+    conn = None
+    loc_id = None
+    try:
+        # connect to the PostgreSQL database
+        conn = get_conn()
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        #cur.execute(insert_query, (name, f"MULTIPOINT(({loc}))"))
+        cur.execute(insert_query, (name, psycopg2.Binary(loc)))
+        # get the generated id back
+        loc_id = cur.fetchone()[0]
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return loc_id
+    
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/scripts.js
@@ -0,0 +1,89 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let trackingInterval;
+    let isTracking = false;
+    const dataPoints = [];
+
+    function updatePosition() {
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+
+                // Update the HTML elements with the latitude and longitude values
+                document.getElementById('latitude').textContent = latitude;
+                document.getElementById('longitude').textContent = longitude;
+
+                // Record the data point
+                if (isTracking) {
+                    dataPoints.push({longitude, latitude});
+                }
+            },
+            function (error) {
+                console.error('Error getting location:', error.message);
+            }
+        );
+    }
+
+    function startTracking() {
+        if (!isTracking) {
+            isTracking = true;
+            dataPoints.length = 0; // Clear previous data points
+            document.getElementById('status').textContent = 'Tracking...';
+            trackingInterval = setInterval(updatePosition, 1000); // Update every 5 seconds
+        }
+    }
+
+    function stopTracking() {
+        if (isTracking) {
+            isTracking = false;
+            document.getElementById('status').textContent = 'Not tracking';
+            clearInterval(trackingInterval);
+            // Send the recorded data to Flask backend
+            sendDataToFlask();
+        }
+    }
+
+    function sendDataToFlask() {
+        // Use fetch API to send data to Flask
+        //let textData = dataPoints.pop().toString();
+        //let count = 1;
+       // while (dataPoints.length > 0) {
+       //     if (count % 2 === 0) {
+       //         textData = dataPoints.pop() + ', ' + textData;
+       //     } else {
+       //         textData = dataPoints.pop() + textData;
+       //     }
+       //     count++;
+       //     };
+
+       // const jsonString = JSON.stringify(dataPoints);
+        // Parse the JSON string into a JavaScript array
+        //const dataArray = JSON.parse(jsonString);
+
+        // Convert the array of objects into the desired format
+        //const formattedString = dataArray
+        //.map(({ longitude, latitude }) => `${longitude} ${latitude}`)
+        //.join(', ');
+       
+        console.log(JSON.stringify(dataPoints));
+        //console.log(formattedString);
+
+        fetch('/process_data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',//'text/plain',
+            },
+            body: JSON.stringify(dataPoints), // formattedString,
+        })
+        .then(response => response.text())
+        .then(data => {
+            console.log('Data sent to Flask:', data);
+        })
+        .catch(error => {
+            console.error('Error sending data to Flask:', error);
+        });
+    }
+
+    document.getElementById('startButton').addEventListener('click', startTracking);
+    document.getElementById('stopButton').addEventListener('click', stopTracking);
+});
